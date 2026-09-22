00ab73331021b6a4324b658feb091391b54fcaab
entityJY <jomathya07@gmail.com>
2026-09-21 17:04:18 -0700

    feat: add credits

--- ui/start_game.gd
+++ ui/start_game.gd
@@ -6,7 +6,6 @@ func _init() -> void:
 	SceneTransition.fade_in()
 
 func _on_button_button_down() -> void:
-	print("fade out")
 	SceneTransition.fade_out()
 	await SceneTransition.animation_finished
 	get_tree().change_scene_to_packed(game_scene)
--- ui/ui.gd
+++ ui/ui.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
-
+@export var credits: RichTextLabel
+var credits_toggled: bool = false
 signal restart_button_pressed()
 
 # Called when the node enters the scene tree for the first time.
@@ -10,8 +11,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("escape"):
+		var tween = get_tree().create_tween()
+		tween.tween_property(credits, "position", Vector2(251, 174), .5)
+		credits_toggled = false
 
 
 func _on_button_button_down() -> void:
 	restart_button_pressed.emit()
+
+
+func _on_credits_button_down() -> void:
+	var tween = get_tree().create_tween()
+	if credits_toggled:
+		tween.tween_property(credits, "position", Vector2(251, -360), .5)
+	else:
+		tween.tween_property(credits, "position", Vector2(251, 225), .5)
+	credits_toggled = !credits_toggled
